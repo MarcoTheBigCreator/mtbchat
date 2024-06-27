0dd394e8cef0e488cafce59047c8acfcb1d4adc1
Karely Durán <marcotbcreator@gmail.com>
2024-06-27 12:19:05 -0600

    feat: Add DashboardLayout component
    The commit adds the `DashboardLayout` component to `src/app/dashboard/dashboard/layout.tsx`. This component represents the layout for the dashboard page, including a sidebar with options and the main content area. It also handles authentication and redirects if the user is not logged in.
    
    This change enhances the application by providing a structured layout for the dashboard page and improving the user experience.

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,6 @@
 import { auth } from '@/auth.config';
 
 export default async function DashboardPage() {
-  const session = await auth();
-
   return (
     <div>
       <h1>Hello Page</h1>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './add/AddFriendButton';
+export * from './icons/Icons';
 export * from './login/LoginForm';
 export * from './Providers';
 export * from './ui/Button';
