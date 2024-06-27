fd191d1b2e29efbda8dbc9a599ac55edc08933e4
Karely Durán <marcotbcreator@gmail.com>
2024-06-27 13:26:05 -0600

    feat: Add lazy loading for images and update dashboard layout
    Enhance page load performance by implementing lazy loading for images. Update the dashboard layout to include a user profile section with a sign out button. These changes improve the user experience and optimize the application.

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import { auth } from '@/auth.config';
-import { Icon, Icons } from '@/components';
+import { Icon, Icons, SignOutButton } from '@/components';
 
 interface SidebarOption {
   id: number;
@@ -33,7 +34,7 @@ export default async function DashboardLayout({
 
   return (
     <div className="w-full flex h-screen">
-      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-x-auto md:overflow-x-hidden overflow-y-auto border-r border-gray-200 bg-white px-6">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
           <Icons.Logo className="h-8 w-auto text-indigo-600" />
         </Link>
@@ -66,6 +67,27 @@ export default async function DashboardLayout({
                   );
                 })}
               </ul>
+            </li>
+            <li className="-mx-6 mt-auto flex items-center">
+              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
+                <div className="relative h-8 w-8 bg-gray-50">
+                  <Image
+                    fill
+                    referrerPolicy="no-referrer"
+                    className="rounded-full"
+                    src={session.user.image || ''}
+                    alt="Your profile picture"
+                  />
+                </div>
+                <span className="sr-only">Your profile</span>
+                <div className="flex flex-col">
+                  <span aria-hidden="true">{session.user.name}</span>
+                  <span className="text-xs text-zinc-400" aria-hidden="true">
+                    {session.user.email}
+                  </span>
+                </div>
+              </div>
+              <SignOutButton className="h-full aspect-square" />
             </li>
           </ul>
         </nav>
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export * from './add/AddFriendButton';
 export * from './icons/Icons';
 export * from './login/LoginForm';
 export * from './Providers';
+export * from './sign-out/SignOutButton';
 export * from './ui/Button';
 export * from './ui/GoogleLogo';
