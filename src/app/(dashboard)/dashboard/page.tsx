cd9ebb9f1af06857102ee226729da6e6e2bb0188
Karely Durán <marcotbcreator@gmail.com>
2024-06-29 10:33:39 -0600

    add: dashboard main page with recent chats

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,18 +1,90 @@
+import { notFound } from 'next/navigation';
 import { auth } from '@/auth.config';
-import { redirect } from 'next/navigation';
+import { getFriendsByUserId } from '@/actions';
+import { fetchRedis } from '@/helpers';
+import { chatHrefConstructor } from '@/lib';
+import { titleFont } from '@/config/fonts';
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default async function DashboardPage() {
+export default async function Home() {
   const session = await auth();
 
-  // just in case middleware fails
   if (!session) {
-    redirect('/login');
+    notFound();
   }
 
+  const friends = await getFriendsByUserId(session.user.id);
+
+  const friendsWithLastMessage = await Promise.all(
+    friends.map(async (friend) => {
+      const [lastMessageRaw] = (await fetchRedis(
+        'zrange',
+        `chat:${chatHrefConstructor(session.user.id, friend.id)}:messages`,
+        -1,
+        -1
+      )) as string[];
+
+      const lastMessage = JSON.parse(lastMessageRaw) as Message;
+
+      return {
+        ...friend,
+        lastMessage,
+      };
+    })
+  );
+
   return (
-    <div>
-      <h1>Hello Page</h1>
-      {/* <pre>{JSON.stringify(session)}</pre> */}
+    <div className="container py-12">
+      <h1 className={`${titleFont.className} font-bold text-5xl mb-8`}>
+        Recent chats
+      </h1>
+      {friendsWithLastMessage.length === 0 ? (
+        <p className="text-base text-zinc-500">Nothing to show here...</p>
+      ) : (
+        friendsWithLastMessage.map((friend) => (
+          <div
+            key={friend.id}
+            className="relative bg-zinc-50 border border-zinc-200 p-3 rounded-md"
+          >
+            <div className="absolute right-4 inset-y-0 flex items-center">
+              <ChevronRight className="h-7 w-7 text-zinc-400" />
+            </div>
+            <Link
+              href={`/dashboard/chat/${chatHrefConstructor(
+                session.user.id,
+                friend.id
+              )}`}
+              className="relative sm:flex"
+            >
+              <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
+                <div className="relative h-6 w-6">
+                  <Image
+                    referrerPolicy="no-referrer"
+                    className="rounded-full"
+                    alt={`${friend.name} profile picture`}
+                    src={friend.image}
+                    fill
+                  />
+                </div>
+              </div>
+
+              <div>
+                <h4 className="text-lg font-semibold">{friend.name}</h4>
+                <p className="mt-1 max-w-md">
+                  <span className="text-violet-500">
+                    {friend.lastMessage.senderId === session.user.id
+                      ? 'You: '
+                      : ''}
+                  </span>
+                  {friend.lastMessage.text}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))
+      )}
     </div>
   );
 }
